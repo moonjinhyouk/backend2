c8fbcf27696a7bd9458232384e10f27f162f03dd
moonjinhyouk <100898147+moonjinhyouk@users.noreply.github.com>
2025-12-11 09:36:32 +0900

    Delete board-backend directory

--- board-backend/src/main/java/com/example/board/repository/ReplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.board.repository;
-
-import com.example.board.entity.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ReplyRepository extends JpaRepository<Reply, Integer> {
-    List<Reply> findByBcode(Integer bcode);
-}
